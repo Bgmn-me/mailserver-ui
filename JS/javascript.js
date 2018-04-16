233e835818aef866d2b7f5099550144de8b015a8
Produxer <Dominik07.98@live.de>
2018-04-16 20:18:17 +0200

    Update OnBirthMonth() function

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,23 +1,39 @@
 function onBirthMonth(birthmonth) {
-    //month = document.forms["register"].elements["birthmonth"].value;
-    //document.getElementById("output").innerHTML = month;
     if (birthmonth == 2){ //gibt an welcher Monat Februar ist.
-        document.getElementById("output").innerHTML = "Birthmonth: " + birthmonth + " hat 28 Tage, birthmonth%2: " + birthmonth%2;
+
+        //check if the selected year is a leap-year or not.
+        byear = document.forms["register"].elements["birthyear"].value;
+        //document.getElementById("output").innerHTML = byear;
+        byearsubstr01 = parseInt((byear).toString().substr(3,1));
+        if(((byearsubstr01 == 0 || byearsubstr01 == 4 || byearsubstr01 == 8) && parseInt((byear).toString().substr(2,1))%2 == 0) || ((byearsubstr01 == 2 || byearsubstr01 == 6) && parseInt((byear).toString().substr(2,1))%2 != 0)){
+            leapyear = true;
+        } else {
+            leapyear = false;
+        }
+        
+        //disable all days over 28(29 by a leap-year) because it is februar.
         for (i = 29; i <= 31 ; i++) {
             if (i == 29 && leapyear == true){
+                document.getElementById("bday29").disabled = false;
             } else {
                 eval("document.getElementById('bday" + i + "').disabled = true");
             }
         }
     }
-    else if (birthmonth <=6 && birthmonth%2 == 0 || birthmonth >= 9 && birthmonth%2 > 0){ //gibt aus welche Monate 30 Tage hat.
-        document.getElementById("output").innerHTML = "Birthmonth: " + birthmonth + " hat 30 Tage, birthmonth%2: " + birthmonth%2;
+    else if (birthmonth <=6 && birthmonth%2 == 0 || birthmonth >= 9 && birthmonth%2 > 0){ //shows which months didn't have more than 30 days and disable the option in the <select> form element.
+        document.getElementById("bday29").disabled = false;
+        document.getElementById("bday30").disabled = false;
         document.getElementById("bday31").disabled = true;
     }
-    else if (birthmonth%2 > 0 && birthmonth <= 7 || birthmonth%2 == 0 && birthmonth >= 8){ // gibt aus welche Monate 31 Tage hat.
-        document.getElementById("output").innerHTML = "Birthmonth: " + birthmonth + " hat 31 Tage, birthmonth%2: " + birthmonth%2;
+    else {// if (birthmonth%2 > 0 && birthmonth <= 7 || birthmonth%2 == 0 && birthmonth >= 8) { //shows which months didn't have more than 31 days and enable the options in the <select> form element.
+        document.getElementById("bday29").disabled = false;
+        document.getElementById("bday30").disabled = false;
+        document.getElementById("bday31").disabled = false;
     }
 }
-//2016, 2012, 2008, 2004, 2000, 1996, 1992, 1988, 1984, 1980
-//gerade 0, 4, 8 ungerade 2, 6
-//2018, 
+
+function onBirthDate(birth,birthdate) {
+    birth = birth;
+    document.getElementById("output").innerHTML = birthdate;
+
+}
